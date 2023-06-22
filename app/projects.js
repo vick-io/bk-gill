f5d19d83916d884da26004bdb570793c01a882a5
bk-gill <132428715+bk-gill@users.noreply.github.com>
2023-06-21 23:52:47 -0400

    Repurposed repo to Git README Profile

--- app/projects.js
+++
@@ -1,8 +0,0 @@
-export default function Projects() {
-  return (
-    <div>
-      <h1>Projects</h1>
-      {/* Add your projects content here */}
-    </div>
-  );
-}
--- app/resume.js
+++
@@ -1,9 +0,0 @@
-export default function Projects() {
-    return (
-      <div>
-        <h1>Resume</h1>
-        {/* Add your projects content here */}
-      </div>
-    );
-  }
-  
